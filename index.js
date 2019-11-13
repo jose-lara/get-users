0ae378c7e34f3989b3d25bd369020fba0aacc655
Jose Lara <jose.laradelamo@axa.com>
2019-11-13 20:09:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const handlerFunction = async (event, context, callback) => {
+  try {
+    const options = {
+      TableName: 'user',
+      FilterExpression: '#active = :active',
+      ExpressionAttributeValues: {
+        ':active': true
+      },
+      ExpressionAttributeNames: {
+        '#active': 'active'
+      }
+    };
+    const user = await docClient.scan(options).promise();
+    const result = {
+      statusCode: 200,
+      body: JSON.stringify(user.Items),
+      headers: { 'content-type': 'application/json' }
+    };
+
+    callback(null, result);
+  } catch (e) {
+    return context.fail(e);
+  }
+};
+exports.handler = handlerFunction;
