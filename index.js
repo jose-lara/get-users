9296ddd0b6d67823e1702105b8fb6e322a3a0151
Luis Prieto <luis.prieto@axa.com>
2019-11-15 13:43:46 +0100

    sort users by surname

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const handlerFunction = async (event, context, callback) => {
   try {
     const options = {
-      TableName: 'User',
-      FilterExpression: '#active = :active',
+      TableName: "User",
+      FilterExpression: "#active = :active",
       ExpressionAttributeValues: {
-        ':active': true
+        ":active": true
       },
       ExpressionAttributeNames: {
-        '#active': 'active'
+        "#active": "active"
       }
     };
     const user = await docClient.scan(options).promise();
+    const sorted = user.Items.sort((a, b) => {
+      if (a.userSurname > b.userSurname) {
+        return 1;
+      }
+      if (a.userSurname < b.userSurname) {
+        return -1;
+      }
+      return 0;
+    });
     const result = {
       statusCode: 200,
-      body: JSON.stringify(user.Items),
-      headers: { 'content-type': 'application/json' }
+      body: JSON.stringify(sorted),
+      headers: { "content-type": "application/json" }
     };
 
     callback(null, result);
